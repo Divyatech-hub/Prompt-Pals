4378d499e51eb64402e712ff6e9c719d08d37ca1
Zooomulus <andelisun@gmail.com>
2023-10-18 20:30:59 -0400

    Update ContentView.swift
    intro page erasure: focus on characters screen select

--- ContentView.swift
+++ ContentView.swift
@@ -22,20 +22,3 @@ struct ContentView: View {
 #Preview {
     ContentView()
 }
-
-//from the main page, swipe up to go to another page called CharacterView (CharacterView needs to be defined initialized)
-class ViewController: UIViewController {
-    @IBOutlet var swipeLabel: UILabel!
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        self.view.backgroundColor = .systemGray
-        
-        let swipeUp = UISwipeGestureRecognizer(target: self, action: #selector(swipedUp))
-        swipeUp.direction = .up
-        self.view.addGestureRecognizer(swipeUp)
-    }
-    @objc func swipedUp() {
-        let vc = CharacterViewController()
-        self.present(vc, animated: true, completion:nil)
-    }
-}
